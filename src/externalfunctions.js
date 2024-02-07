8af37d0574ca589701fc3f4d0bf05d467f7dfdd3
Nolan Nguyen! <nolan2087@gmail.com>
2024-02-07 12:26:29 -0800

    adding some interesting features and playing around with having the bot respond to server messages

---
+++ src/externalfunctions.js
@@ -0,0 +1,5 @@
+function randomNum(chanceTotal) {
+    var num = Math.floor(Math.random() * chanceTotal);
+    console.log(num);
+    return num;
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
+/* 
+    Thank you to Under Ctrl on YouTube and taniarascia.com for guidance on Discord Bots and
+    connecting to APIs with JavaScript.
+*/
+
 require('dotenv').config();
+// var externalfunctions = require('./externalfunctions.js')
+var XMLHttpRequest = require('xhr2').XMLHttpRequest;
+
+var numTries = 1;
+const RATE = 100;
+// Stupid random number function.
+function randomNum(chanceTotal) {
+    var num = Math.floor(Math.random() * chanceTotal);
+    console.log(num);
+    return num;
+}
 
 const { Client, IntentsBitField, InteractionCollector } = require('discord.js');
 
@@ -28,10 +44,47 @@ ftm.on('messageCreate', (message) => {
         return;
     }
 
+    if (message.author.username === 'crayollaaaa') {
+        if (randomNum(RATE) === 1) {
+            message.reply(`WIN! at a rate of 1 in ${RATE}, that took you ${numTries} tries.`);
+            numTries = 1;
+            console.log("did it.");
+        } else {
+            numTries++;
+        }
+        return;
+    }
+
     if (message.content === 'how many free throws does joel embiid have right now') {
         message.reply('too many.');
         // console.log(message);
     }
 });
+
+// async function fetchFreeThrows() {
+//     const response = await fetch('https://www.balldontlie.io/api/v1/teams');
+//     const data = await response.json();
+// }
+
+let request = new XMLHttpRequest();
+request.open('GET', 'https://www.balldontlie.io/api/v1/teams', true);
+request.onload = function () {
+
+    var data = JSON.parse(this.response);
+
+    if (request.status >= 200 && request.status < 400) {
+        for (const team in data) {
+            console.log(team.full_name)
+        }
+    } else {
+        console.log('error');
+    }
+
+}
+
+request.send();
+
+
+
 
 ftm.login(process.env.TOKEN);
